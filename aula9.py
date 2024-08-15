56412b2fb7db81e6e0edda625706c411f95688a9
letlet12312 <leticia.maria.santos100@gmail.com>
2024-08-15 16:12:18 -0300

    Add files via upload

---
+++ aula9.py
@@ -0,0 +1,121 @@
+
+# print('E-commerce X')
+# nome = input('Digite seu nome: ')
+# usuario = input('Digite seu usuario: ')
+# email = input('Digite seu e-mail: ')
+# senha = input('Digite seu senha: ')
+
+# print('olá', nome, 'seu usuário é ', usuario)
+
+# senha_dig = input('Senha: ')
+# usuario_dig = input('Usuário: ')
+
+# if  senha_dig == senha and usuario_dig == usuario: 
+#     print('Seja bem vindo(a)', nome, 'faça seu pedido:')
+#     menu = {
+#      'camiseta': 150.00,
+#      'meia': 50.00,
+#      'computador': 20.000,
+#      'fone': 100.00
+
+#     }
+#     print('produtos: ', menu )
+    
+#     meus_produtos  = []
+#     total_pagar  = []
+#     pedido1 = input('digite o produto')
+#     pedido2 = input('digite o produto')
+#     pedido3 = input('digite o produto')
+    
+#     meus_produtos +=(pedido1, pedido2, pedido3)
+#     total_pagar  += [menu[pedido1], menu[pedido2], menu[pedido3]]
+
+#     print('SEUS PEDIDOS:')
+#     print(meus_produtos)
+#     print(total_pagar)
+    
+#     soma =  sum(total_pagar)
+#     float(soma)
+#     print('TOTAL DA COMPRA:')
+#     print('R$', soma)
+
+#     forma_pag = input('''
+#     ESCOLHA A FORMA DE PAGAMENTO:
+#     1 -  PIX
+#     2 - CC
+#     3 - PAYPALL
+       
+#     ''')
+
+#     if forma_pag == '1':
+#         print('Seu pagamento foi efetuado com PIX: R$ ', soma )
+#     elif forma_pag == '2':
+#         print('Seu pagamento foi efetuado com CC: R$ ', soma )
+#     elif forma_pag == '3':
+#         print('Seu pagamento foi efetuado com PYPALL: R$ ', soma )   
+#     else: 
+#         print('digite algo valido')    
+
+# else: 
+#     print('Algo foi digitado errado')    
+
+
+# usuario_dig =input('Usuário: ')
+
+
+# atividade 2
+
+# cliente1= input('Digite seu nome:')
+# cliente1_idade= input('Digite sua idade')
+
+# cliente2= input('Digite seu nome:')
+# cliente2_idade= int (input('Digite sua idade'))
+
+# cliente3= input('Digite seu nome:')
+# cliente3_idade= int (input('Digite sua idade'))
+
+# Quarto_cliente1= input('escolha os quarto ("Simples", "Duplo" e "Luxo".)').strip().lower()
+# Quarto_cliente2=input('escolha os quarto ("Simples", "Duplo" e "Luxo".)').strip().lower()
+# Quarto_cliente3=input('escolha os quarto ("Simples", "Duplo" e "Luxo".)').strip().lower()
+
+# dias_cliente1= int(input('escolha quantos dias'))
+# dias_cliente2= int(input('escolha quantos dias'))
+# dias_cliente3= int(input('escolha quantos dias'))
+
+# preço_simples= 100.00
+# preço_duplo= 150.00
+# preço_luxo= 250.00
+
+# if Quarto_cliente1 == 'simples':
+#  valor_cliente1= preço_simples * dias_cliente1
+# elif Quarto_cliente1 == 'duplo':
+#  valor_cliente1= preço_duplo * dias_cliente1
+# elif Quarto_cliente1 == 'luxo':
+#   valor_cliente1= preço_luxo * dias_cliente1
+# else: 
+#   valor_cliente1=0
+
+# if Quarto_cliente2 == 'simples':
+#  valor_cliente2= preço_simples * dias_cliente2
+# elif Quarto_cliente2 == 'duplo':
+#  valor_cliente2= preço_duplo * dias_cliente2
+# elif Quarto_cliente2 == 'luxo':
+#   valor_cliente2= preço_luxo * dias_cliente2
+# else: 
+#   valor_cliente2=0
+  
+# if Quarto_cliente3== 'simples':
+#  valor_cliente3= preço_simples * dias_cliente3
+# elif Quarto_cliente3 == 'duplo':
+#  valor_cliente3= preço_duplo * dias_cliente3
+# elif Quarto_cliente3 == 'luxo':
+#   valor_cliente3= preço_luxo * dias_cliente3
+# else: 
+#   valor_cliente3=0
+
+# print(f'\nresumo:')
+# print(f"cliente_1 ({cliente1}): R$ {valor_cliente1:.2f}")
+# print(f"cliente_2 ({cliente2}): R$ {valor_cliente2:.2f}")
+# print(f"cliente_3 ({cliente3}): R$ {valor_cliente3:.2f}")
+
+  
